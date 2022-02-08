cfa82a75e3e43524a07ec9e76e2a9bb115e1c5dd
Mukahhal <mukahhal@umich.edu>
2022-02-08 12:33:12 -0500

    Login added

--- src/SingleServerJPB1.java
+++ src/SingleServerJPB1.java
@@ -1,10 +1,8 @@
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -38,6 +36,16 @@ public class SingleServerJPB1 {
 
             File loginFile = new File(
                     "Database/login.txt");
+            BufferedReader br
+                    = new BufferedReader(new FileReader(loginFile));
+            ArrayList<String> loginArray = new ArrayList<>();
+            // Declaring a string variable
+            String loginString;
+            // Condition holds true till
+            // there is character in a string
+            while ((loginString = br.readLine()) != null)
+                // Print the string
+                loginArray.add(loginString);
             
             //server loop listening for the client 
             //and responding
@@ -47,15 +55,23 @@ public class SingleServerJPB1 {
                 String command = data[0];
                 String param1 = "";
                 String param2 = "";
-                if(data.length > 0){
+                if(data.length > 1){
                     param1 = data[1];
-                    if(data.length > 1){
+                    if(data.length > 2){
                         param2 = data[2];
                     }
                 }
                 if(command.equalsIgnoreCase("LOGIN")) {
                     System.out.println("Sending Client Login Message");
-                    outputToClient.writeUTF("Login Called for username: " + param1 + " and password: " + param2);
+                    //outputToClient.writeUTF("Login Called for username: " + param1 + " and password: " + param2);
+                    if(loginArray.contains(param1 + " " + param2)){
+                        outputToClient.writeUTF("SUCCESS");
+                        System.out.println( param1 + " Logged in successfully");
+                    }
+                    else{
+                        outputToClient.writeUTF("FAILURE: Please provide correct username and password.  Try again.");
+                        System.out.println( param1 + " Did not login, failed attempt, maybe a scam?");
+                    }
                 }
                 else if(strReceived.equalsIgnoreCase("quit")) {
                     System.out.println("Shutting down server...");
